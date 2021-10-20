57e5f02ed061ed1771323e43d6ba9eaaa625c4b2
Waley-Z <waley_zheng@sjtu.edu.cn>
2021-10-19 22:34:11 -0400

    Add footer

--- script/script.js
+++ script/script.js
@@ -1,4 +1,34 @@
-$(document).ready(function() {
-    $('li.active').removeClass('active');
-    $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
-});
+ /* Code for changing active link on clicking */
+ var btns =
+     $(".navbar-nav .nav-link");
+
+ for (var i = 0; i < btns.length; i++) {
+     btns[i].addEventListener("click",
+         function() {
+             var current = document
+                 .getElementsByClassName("active");
+
+             current[0].className = current[0]
+                 .className.replace(" active", "");
+
+             this.className += " active";
+         });
+ }
+
+ /* Code for changing active 
+ link on Scrolling */
+ $(window).scroll(function() {
+     var distance = $(window).scrollTop();
+     $('.page-section').each(function(i) {
+
+         if ($(this).position().top <=
+             distance + 250) {
+
+             $('.navbar-nav a.active')
+                 .removeClass('active');
+
+             $('.navbar-nav a').eq(i)
+                 .addClass('active');
+         }
+     });
+ }).scroll();
